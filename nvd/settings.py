a6a647c96752c59efb715c61f489fe0573cc9d91
shane-thomas-pai <shane.thomas@prevalent.ai>
2024-05-15 12:52:08 +0530

    AR-65: Formatted settings.py

--- nvd/settings.py
+++ nvd/settings.py
@@ -16,17 +16,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -111,7 +108,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'nvd.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -122,10 +118,9 @@ DATABASES = {
         'USER': 'postgres',
         'HOST': 'host.docker.internal',
         'PORT': 5432,
-        'PASSWORD':'shane'
+        'PASSWORD': 'shane'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -145,7 +140,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -156,7 +150,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
